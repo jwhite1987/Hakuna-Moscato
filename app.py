0eb801954963997b512272ae40615d9626c7d02c
Jimmy <>
2021-05-26 19:34:58 -0400

    new

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
 from flask import Flask, render_template, redirect, url_for
-from flask_pymongo import PyMongo
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 
 # Use PyMongo to establish Mongo connection
-app.config["MONGO_URI"] = "mongodb+srv://jimmywhite87:Ruger2012@cluster0.kmis8.mongodb.net/pitchfork-review?retryWrites=true&w=majority"
-mongo = PyMongo(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:Ruger2012@uncc-database.cdwa3ro17u26.us-east-2.rds.amazonaws.com:5432/postgres"
+db = SQLAlchemy(app)
 # Route to render index.html template using data from Mongo
 @app.route("/")
 def home():
 
-    artists = mongo.db.content.find()
+    # artists = mongo.db.content.find()
 
     # Return template and data
-    return render_template("index.html", artists=artists)
+    return render_template("index.html")
 
 
 @app.route("/icons")
