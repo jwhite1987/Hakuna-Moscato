381e829a6eb6cf4cf0e45aecb93a3e6a33541ced
CameronFarquhar <cameronfarquhar@gmail.com>
2021-06-11 19:37:50 -0400

    push

--- static/js/chart-page.js
+++ static/js/chart-page.js
@@ -4,7 +4,7 @@ var ctx = document.getElementById('pie-chart');
 function init() {
 
 d3.csv("static/data/gws_cleaned_dataset.csv").then((d)=> {
-        // loop through names object and grab all the ids/names and eppend them to demographics dropdown
+// loop through names object and grab all the ids/names and eppend them to demographics dropdown
 
 const distinctCountries = [...new Set(d.map(x => x.country))];
 
@@ -16,14 +16,17 @@ const distinctCountries = [...new Set(d.map(x => x.country))];
         }
 
         // // apply an ID for the intial plots
-        piechart(distinctCountries[1]);
-
+        piechart(distinctCountries[3]);
+        barchart(distinctCountries[3]);
+        linechart(distinctCountries[3]);
     });
 };
 
-    function optionChanged(Country){
-        piechart(Country);
-      };
+function optionChanged(Country){
+piechart(Country);
+barchart(Country);
+linechart(Country);
+};
       
 
 
@@ -75,159 +78,160 @@ d3.csv('static/data/gws_cleaned_dataset.csv').then(d => {
 }
 
 
+function barchart(Country){
+d3.csv('static/data/gws_cleaned_dataset.csv').then(d => {
+        var btx = document.getElementById('bar-chart')
+        var countryFilter = d.filter(d=>d.country === Country);
 
-// d3.csv('static/data/gws_cleaned_dataset.csv').then(d => {
-//         var btx = document.getElementById('bar-chart')
-//         var countryFilter = d.filter(d=>d.country === 'Usa');
+        var redRating = [];
+        var whiteRating = [];
+        // var age = [];
 
-//         var redRating = [];
-//         var whiteRating = [];
-//         // var age = [];
+        countryFilter.forEach(function(data){
+                if (data.color == "Red"){
+                        redRating.push(data.score);
+                }
+                else if (data.color == "White"){
+                        whiteRating.push(data.score);
+                }
+        })
 
-//         countryFilter.forEach(function(data){
-//                 if (data.color == "Red"){
-//                         redRating.push(data.score);
-//                 }
-//                 else if (data.color == "White"){
-//                         whiteRating.push(data.score);
-//                 }
-//         })
+        function sum( obj ) {
+                var sum = 0;
+                for( var el in obj ) {
+                  if( obj.hasOwnProperty( el ) ) {
+                    sum += parseFloat( obj[el] );
+                  }
+                }
+                return sum;
+              }
+              var redRatingAvg = sum(redRating)/redRating.length;
+              var whiteRatingAvg = sum(whiteRating)/whiteRating.length;
 
-//         function sum( obj ) {
-//                 var sum = 0;
-//                 for( var el in obj ) {
-//                   if( obj.hasOwnProperty( el ) ) {
-//                     sum += parseFloat( obj[el] );
-//                   }
-//                 }
-//                 return sum;
-//               }
-//               var redRatingAvg = sum(redRating)/redRating.length;
-//               var whiteRatingAvg = sum(whiteRating)/whiteRating.length;
-
-//               console.log( " Red: "+redRatingAvg );
-//               console.log( " White: "+whiteRatingAvg );
-
-
-//               var chart = new Chart(btx, {
-//               type: 'bar',
-//               data: {
-//                 labels: ["Red", "White"],
-//                 datasets: [
-//                   {
-//                     label: "Average Score",
-//                     backgroundColor: ["#722f37", "#EEEDC4"],
-//                     data: [redRatingAvg,whiteRatingAvg]
-//                   }
-//                 ]
-//               },
-//               options: {
-//                 legend: { display: false },
-//                 title: {
-//                   display: true,
-//                   text: 'Red and White Wine Scores'
-//                 }
-//               }
-//           });
-
-// });
+              console.log( " Red: "+redRatingAvg );
+              console.log( " White: "+whiteRatingAvg );
 
 
-// d3.csv('static/data/gws_cleaned_dataset.csv').then(d => {
-//         var atx = document.getElementById('line-chart')
-//         var countryFilter = d.filter(d=>d.country === 'Usa');
+              var chart = new Chart(btx, {
+              type: 'bar',
+              data: {
+                labels: ["Red", "White"],
+                datasets: [
+                  {
+                    label: "Average Score",
+                    backgroundColor: ["#722f37", "#EEEDC4"],
+                    data: [redRatingAvg,whiteRatingAvg]
+                  }
+                ]
+              },
+              options: {
+                legend: { display: false },
+                title: {
+                  display: true,
+                  text: 'Red and White Wine Scores'
+                }
+              }
+          });
 
-//         var Rating100 = [];
-//         var Rating90 = [];
-//         var Rating80 = []
-//         var Rating70 = []
-//         var Rating60 = []
-//         var Rating50 = []
-//         var Rating40 = []
-//         var Rating30 = []
-//         var Rating20 = []
-//         var Rating10 = []
-//         var Rating1 = []
+});
+}
 
-//         countryFilter.forEach(function(data){
-//                 if (data.wine_age > 100){
-//                         Rating100.push(data.score);
-//                 }
-//                 else if (data.wine_age > 90){
-//                         Rating90.push(data.score);
-//                 }
-//                 else if (data.wine_age > 80){
-//                         Rating80.push(data.score);
-//                 }
-//                 else if (data.wine_age > 70){
-//                         Rating70.push(data.score);
-//                 }
-//                 else if (data.wine_age > 60){
-//                         Rating60.push(data.score);
-//                 }
-//                 else if (data.wine_age > 50){
-//                         Rating50.push(data.score);
-//                 }
-//                 else if (data.wine_age > 40){
-//                         Rating40.push(data.score);
-//                 }
-//                 else if (data.wine_age > 30){
-//                         Rating30.push(data.score);
-//                 }
-//                 else if (data.wine_age > 20){
-//                         Rating20.push(data.score);
-//                 }
-//                 else if (data.wine_age > 10){
-//                         Rating10.push(data.score);
-//                 }
-//                 else if (data.wine_age >= 1){
-//                         Rating1.push(data.score);
-//                 }
-//         })
+function linechart(Country){
+d3.csv('static/data/gws_cleaned_dataset.csv').then(d => {
+        var atx = document.getElementById('line-chart')
+        var countryFilter = d.filter(d=>d.country === Country);
 
-//         function sum( obj ) {
-//                 var sum = 0;
-//                 for( var el in obj ) {
-//                   if( obj.hasOwnProperty( el ) ) {
-//                     sum += parseFloat( obj[el] );
-//                   }
-//                 }
-//                 return sum;
-//               }
-//               var Rating100avg = sum(Rating100)/Rating100.length;
-//               var Rating90avg =sum(Rating90)/Rating90.length;
-//               var Rating80avg =sum(Rating80)/Rating80.length;
-//               var Rating70avg = sum(Rating70)/Rating70.length;
-//               var Rating60avg = sum(Rating60)/Rating60.length;
-//               var Rating50avg = sum(Rating50)/Rating50.length;
-//               var Rating40avg = sum(Rating40)/Rating40.length;
-//               var Rating30avg = sum(Rating30)/Rating30.length;
-//               var Rating20avg = sum(Rating20)/Rating20.length;
-//               var Rating10avg = sum(Rating10)/Rating10.length;
-//               var Rating1avg =sum(Rating1)/Rating1.length;
+        var Rating100 = [];
+        var Rating90 = [];
+        var Rating80 = []
+        var Rating70 = []
+        var Rating60 = []
+        var Rating50 = []
+        var Rating40 = []
+        var Rating30 = []
+        var Rating20 = []
+        var Rating10 = []
+        var Rating1 = []
 
-//               console.log(Rating1avg);
+        countryFilter.forEach(function(data){
+                if (data.wine_age > 100){
+                        Rating100.push(data.score);
+                }
+                else if (data.wine_age > 90){
+                        Rating90.push(data.score);
+                }
+                else if (data.wine_age > 80){
+                        Rating80.push(data.score);
+                }
+                else if (data.wine_age > 70){
+                        Rating70.push(data.score);
+                }
+                else if (data.wine_age > 60){
+                        Rating60.push(data.score);
+                }
+                else if (data.wine_age > 50){
+                        Rating50.push(data.score);
+                }
+                else if (data.wine_age > 40){
+                        Rating40.push(data.score);
+                }
+                else if (data.wine_age > 30){
+                        Rating30.push(data.score);
+                }
+                else if (data.wine_age > 20){
+                        Rating20.push(data.score);
+                }
+                else if (data.wine_age > 10){
+                        Rating10.push(data.score);
+                }
+                else if (data.wine_age >= 1){
+                        Rating1.push(data.score);
+                }
+        })
 
-//                 new Chart(document.getElementById("line-chart"), {
-//                 type: 'line',
-//                 data: {
-//                   labels: [100,90,80,70,60,50,40,30,20,10,1],
-//                   datasets: [{ 
-//                       data: [Rating100avg,Rating90avg,Rating80avg,Rating70avg,Rating60avg,Rating50avg,Rating40avg,Rating30avg,Rating20avg,Rating10avg,Rating1avg],
-//                       label: "Average Wine Score by Age",
-//                       borderColor: "#722f37",
-//                       fill: false
-//                     }
-//                   ]
-//                 },
-//                 options: {
-//                   title: {
-//                     display: true,
-//                     text: 'World population per region (in millions)'
-//                   }
-//                 }
-//               });
+        function sum( obj ) {
+                var sum = 0;
+                for( var el in obj ) {
+                  if( obj.hasOwnProperty( el ) ) {
+                    sum += parseFloat( obj[el] );
+                  }
+                }
+                return sum;
+              }
+              var Rating100avg = sum(Rating100)/Rating100.length;
+              var Rating90avg =sum(Rating90)/Rating90.length;
+              var Rating80avg =sum(Rating80)/Rating80.length;
+              var Rating70avg = sum(Rating70)/Rating70.length;
+              var Rating60avg = sum(Rating60)/Rating60.length;
+              var Rating50avg = sum(Rating50)/Rating50.length;
+              var Rating40avg = sum(Rating40)/Rating40.length;
+              var Rating30avg = sum(Rating30)/Rating30.length;
+              var Rating20avg = sum(Rating20)/Rating20.length;
+              var Rating10avg = sum(Rating10)/Rating10.length;
+              var Rating1avg =sum(Rating1)/Rating1.length;
+              console.log(Rating1avg);
+
+                new Chart(document.getElementById("line-chart"), {
+                type: 'line',
+                data: {
+                  labels: [100,90,80,70,60,50,40,30,20,10,1],
+                  datasets: [{ 
+                      data: [Rating100avg,Rating90avg,Rating80avg,Rating70avg,Rating60avg,Rating50avg,Rating40avg,Rating30avg,Rating20avg,Rating10avg,Rating1avg],
+                      label: "Average Wine Score by Age",
+                      borderColor: "#722f37",
+                      fill: false
+                    }
+                  ]
+                },
+                options: {
+                  title: {
+                    display: true,
+                    text: 'World population per region (in millions)'
+                  }
+                }
+              });
               
-// });
+});
+}
 
 init();
