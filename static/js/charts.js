4138a332fc78f9c7de009b3aa5b29148ac4125a5
Jimmy <>
2021-06-11 00:38:17 -0400

    Update charts.js

--- static/js/charts.js
+++ static/js/charts.js
@@ -37,7 +37,7 @@ d3.csv('static/data/gws_cleaned_dataset.csv').then(d => {
       //     borderWidth: 1,
       // },
       // {
-        label:'Wine Mag Dataset',
+        label:'Wine Magazine',
         type: 'bar',
         data: count_2,
         backgroundColor: [
@@ -201,3 +201,73 @@ d3.csv('static/data/gws_cleaned_dataset.csv').then(d => {
   }})
 })
 })
+
+
+var ctx_4 = document.getElementById('wineChart');
+  d3.csv('static/data/updated_winery_dataset.csv').then(data => {
+    var winery = [... new Set(data.map(obj=>obj['winery']))]
+    var count_2 = {};
+    data.map(a=> a.winery).forEach(function(i) {count_2[i] = (count_2[i]||0) + 1})
+    var items = Object.keys(count_2).map(function(key) {
+      return [key, count_2[key]];
+    });
+    items.sort(function(first, second) {
+      return second[1] - first[1];
+    });
+    var newData = items.slice(0, 5)
+    console.log(newData[0])
+    var areaChart = new Chart(ctx_4, {
+    data: {
+      datasets: [{
+        type: 'pie',
+        label: 'Wine Magazine',
+        data: newData,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 205, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(54, 162, 235, 0.2)'
+
+        ],
+        borderColor: [
+          'rgb(255, 99, 132)',
+          'rgb(255, 159, 64)',
+          'rgb(255, 205, 86)',
+          'rgb(75, 192, 192)',
+          'rgb(54, 162, 235)',
+          'rgb(153, 102, 255)',
+          'rgb(201, 203, 207)'
+        ],
+        // borderWidth: 1,
+    }]
+    // options: {
+    //   // responsive: true,
+    //   // indexAxis: 'x',
+    //   // elements: {
+    //   //   bar: {
+    //   //     borderWidth: 2,
+    //   //   }
+    //   // },
+    //   // plugins: {
+    //   //   title: {
+    //   //     display: true
+    //   //   }
+    //   // },
+    //   scales: {
+    //     // y: {
+    //     //   beginAtZero: true,
+    //     //   stacked: true
+    //     // },
+    //     x: {
+    //       title: {
+    //         type: 'linear',
+    //         display: true,
+    //         position: 'bottom'
+    //       }
+    //     }
+    //   }
+    // }
+  }})
+})
+// })
