2599670009cb514de5b36d8a6da523b6612012d3
Jimmy <>
2021-06-11 00:35:29 -0400

    push

--- static/js/charts.js
+++ static/js/charts.js
@@ -204,71 +204,70 @@ d3.csv('static/data/gws_cleaned_dataset.csv').then(d => {
 
 
 var ctx_4 = document.getElementById('wineChart');
-d3.csv('static/data/gws_cleaned_dataset.csv').then(d => {
-  var country = [... new Set(d.map(obj=>obj['country']))]
-  var countries = d.map(a=>a['country'])
-  // var total = 0;
-  // for(var i = 0; countries.length; i++){
-  //   total += d.score[i];
-  // }
-  // var avg = total / countries.length
-  var count = {};
-  d.map(a=> a.country).forEach(function(i) {count[i] = (count[i]||0) + 1})
-  console.log(count);
   d3.csv('static/data/updated_winery_dataset.csv').then(data => {
-    var countryNames = [... new Set(data.map(obj=>obj['country']))]
+    var winery = [... new Set(data.map(obj=>obj['winery']))]
     var count_2 = {};
-    data.map(a=> a.country).forEach(function(i) {count_2[i] = (count_2[i]||0) + 1})
-    console.log(count_2);
-    var counting = new Array(count_2)
+    data.map(a=> a.winery).forEach(function(i) {count_2[i] = (count_2[i]||0) + 1})
+    var items = Object.keys(count_2).map(function(key) {
+      return [key, count_2[key]];
+    });
+    items.sort(function(first, second) {
+      return second[1] - first[1];
+    });
+    var newData = items.slice(0, 5)
+    console.log(newData[0])
     var areaChart = new Chart(ctx_4, {
-    type: 'bar',
     data: {
-      labels: [
-        // countryNames
-      ],
       datasets: [{
+        type: 'pie',
         label: 'Wine Magazine',
-        data: count,
+        data: newData,
         backgroundColor: [
-          'rgba(255, 99, 132, 0.2)'
-          // 'rgba(255, 159, 64, 0.2)',
-          // 'rgba(255, 205, 86, 0.2)',
-          // 'rgba(75, 192, 192, 0.2)',
-          // 'rgba(54, 162, 235, 0.2)',
-          // 'rgba(153, 102, 255, 0.2)',
-          // 'rgba(201, 203, 207, 0.2)'
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 205, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(54, 162, 235, 0.2)'
+
         ],
         borderColor: [
-          'rgb(255, 99, 132)'
-          // 'rgb(255, 159, 64)',
-          // 'rgb(255, 205, 86)',
-          // 'rgb(75, 192, 192)',
-          // 'rgb(54, 162, 235)',
-          // 'rgb(153, 102, 255)',
-          // 'rgb(201, 203, 207)'
+          'rgb(255, 99, 132)',
+          'rgb(255, 159, 64)',
+          'rgb(255, 205, 86)',
+          'rgb(75, 192, 192)',
+          'rgb(54, 162, 235)',
+          'rgb(153, 102, 255)',
+          'rgb(201, 203, 207)'
         ],
-        borderWidth: 1,
-    }],
-    options: {
-      indexAxis: 'y', 
-      plugins: {
-        title: {
-          display: true,
-          text: 'Wine Count per Country'
-        }
-      },
-      scales: {
-        y: {
-          beginAtZero: true,
-          stacked: false,
-          title: {
-            display: true,
-            text: 'Number of Wines'
-          }
-        }
-      }
-    }
+        // borderWidth: 1,
+    }]
+    // options: {
+    //   // responsive: true,
+    //   // indexAxis: 'x',
+    //   // elements: {
+    //   //   bar: {
+    //   //     borderWidth: 2,
+    //   //   }
+    //   // },
+    //   // plugins: {
+    //   //   title: {
+    //   //     display: true
+    //   //   }
+    //   // },
+    //   scales: {
+    //     // y: {
+    //     //   beginAtZero: true,
+    //     //   stacked: true
+    //     // },
+    //     x: {
+    //       title: {
+    //         type: 'linear',
+    //         display: true,
+    //         position: 'bottom'
+    //       }
+    //     }
+    //   }
+    // }
   }})
 })
-})
+// })
